c128673a6606b889a1664073a2fe587ccec99317
RaeIz <sebastian.rivas@uao.edu.co>
2026-09-04 21:19:14 -0500

    Agregar test de matriz transpuesta.

--- tests/test_matrix_operations.py
+++ tests/test_matrix_operations.py
@@ -19,3 +19,8 @@ def test_multiply_matrices():
     B = np.array([[1, 2], [3, 4]])
     expected_result = np.array([[5, 12], [21, 32]])
     assert np.array_equal(multiply_matrices(A, B), expected_result)
+    
+def test_transpose_matrix():
+    A = np.array([[1, 2], [3, 4]])
+    expected_result = np.array([[1, 3], [2, 4]])
+    assert np.array_equal(transpose_matrix(A), expected_result)
